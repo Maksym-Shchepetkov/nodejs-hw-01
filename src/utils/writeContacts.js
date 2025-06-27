12f720e657e1814cd89999875bb505e067249182
Maksym Shchepetkov <mama2009113@gmail.com>
2025-06-27 09:43:12 +0300

    fix bugs

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const updatedData = await fs.writeFile(
+    PATH_DB,
+    JSON.stringify(updatedContacts),
+    'utf-8',
+  );
+  return updatedData;
+};
